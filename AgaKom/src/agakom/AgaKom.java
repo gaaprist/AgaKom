756461668df16d98a89046b3ce3744a623e70ac3
gaaprist <38368569+gaaprist@users.noreply.github.com>
2020-05-15 23:27:26 +0700

    Add files via upload

---
+++ AgaKom/src/agakom/AgaKom.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agakom;
+
+/**
+ *
+ * @author Aga
+ */
+public class AgaKom {
+
+    private int total;
+    private int bayar;
+    private int kembali;
+    private int jumlah;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    /*        listdetil.settotal(jtotal);
+        listdetil.setbayar(jbayar);
+        listdetil.setkembali(jkembali);
+        listdetil.setjumlah(rows)
+    
+        public void setMerk(String merk) {
+        this.merk = merk;
+    }
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+    
+        public String getMerk(){
+        return merk;
+    }
+
+*/
+    public void settotal(int jtotal){
+        this.total = total;
+    }
+    public int gettotal (){
+        return total;
+        
+    }
+    
+    
+
+}
